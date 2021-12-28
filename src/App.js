10788dc39c80cfff84c2208a6e03cb0501e69443
pfreitas <pfreitas@sfu.ca>
2021-12-27 21:10:35 -0800

    Fixed problems with footer

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
         <div id="header-items">
           <h1 id="title-text">ZOIR-SITE</h1>
           <div id="header-items-right">
-            <h1 id="home-text">HOME</h1>
-            <h1 id="projects-text">PROJECTS</h1>
-            <h1 id="about-us-text">ABOUT US</h1>
-            <h1 id="header-contact-text">CONTACT</h1>
+            <h1 className="header-button">HOME</h1>
+            <h1 className="header-button">PROJECTS</h1>
+            <h1 className="header-button">ABOUT US</h1>
+            <h1 className="header-button">CONTACT</h1>
           </div>
         </div>
       </div>
@@ -26,9 +26,19 @@ function App() {
               <h1 id="footer-located-in-vancouver">Located in Vancouver</h1>
             </div>
             <div id="footer-inner-body2-right">
-              <h1 id="footer-home-button">HOME</h1>
-              <h1 id="footer-about-us-button">ABOUT US</h1>
-              <h1 id="footer-newsletter-button">NEWSLETTER</h1>
+              <h1
+                id="footer-home-button"
+                className="footer-buttons"
+                onClick={console.log("Clicked home")}
+              >
+                HOME
+              </h1>
+              <h1 id="footer-about-us-button" className="footer-buttons">
+                ABOUT US
+              </h1>
+              <h1 id="footer-newsletter-button" className="footer-buttons">
+                NEWSLETTER
+              </h1>
             </div>
           </div>
         </div>
